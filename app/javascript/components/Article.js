114bfc9afdb06c1cc0fd4768ca721005c81a600e
roclmbr <github@steveadamson.com>
2018-05-29 13:11:02 -0300

    Adding atricle component

---
+++ app/javascript/components/Article.js
@@ -0,0 +1,46 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Timestamp from "react-timestamp"
+
+class Article extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div className='article-title'>
+            <a href={this.props.path}>{this.props.title}</a>
+        </div>
+        <div className='article-body'>
+            {this.props.description}
+            <div className="article-meta-details">
+                <small>Created by: {this.props.author}, &nbsp;
+                       <Timestamp time={this.props.created_at} precision={3} />, 
+                       last updated: <Timestamp time={this.props.updated_at} precision={3}/>
+                </small>
+            </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+    
+    componentDidMount() {
+        var self = this;
+        setInterval(function() { self.forceUpdate() }, 1000)
+    }
+    
+    componentWillUnmount() {
+        if (this._timer) {
+          clearInterval(this._timer);
+          this._timer = null;
+        }
+    }
+}
+
+Article.propTypes = {
+  title: PropTypes.string,
+  path:  PropTypes.string,
+  description: PropTypes.string,
+  author: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string
+};
+export default Article
